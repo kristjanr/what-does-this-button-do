90c34aa8ed81c345aa095ff7645b6d7a4c7c1db7
Kris <kristjan.roosild@pactum.com>
2023-02-15 19:28:14 +0200

    estonishing & fixes

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -10,20 +10,20 @@ async function generate() {
     feed_url: 'https://kristjan.roosild.ee/feed.xml'
   })
 
-  const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
+  const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'ajaveeb'))
   const allPosts = []
   await Promise.all(
     posts.map(async (name) => {
       if (name.startsWith('index.')) return
 
       const content = await fs.readFile(
-        path.join(__dirname, '..', 'pages', 'posts', name)
+        path.join(__dirname, '..', 'pages', 'ajaveeb', name)
       )
       const frontmatter = matter(content)
 
       allPosts.push({
         title: frontmatter.data.title,
-        url: '/posts/' + name.replace(/\.mdx?/, ''),
+        url: '/ajaveeb/' + name.replace(/\.mdx?/, ''),
         date: frontmatter.data.date,
         description: frontmatter.data.description,
         categories: frontmatter.data.categories,
