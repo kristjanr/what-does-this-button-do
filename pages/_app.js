7169cf8fc666dcfc0bb9c320f5e1bff409b55593
Kris <kristjan.roosild@pactum.com>
2023-03-15 12:34:43 +0200

    Revert "Add Youtube embedding component using GPT-4"
    This reverts commit c7669032bc462eb0ba19618563e3e655df64d938.

--- pages/_app.js
+++ pages/_app.js
@@ -1,34 +1,27 @@
 import 'nextra-theme-blog/style.css'
 import Head from 'next/head'
-import { MDXProvider } from '@mdx-js/react'
-import YouTubeComponent from '../components/YouTube'
+
 import '../styles/main.css'
 
-const components = {
-    youtube: YouTubeComponent,
-};
-
 export default function Nextra({ Component, pageProps }) {
-    return (
-        <>
-            <Head>
-                <link
-                    rel="alternate"
-                    type="application/rss+xml"
-                    title="RSS"
-                    href="/feed.xml"
-                />
-                <link
-                    rel="preload"
-                    href="/fonts/Inter-roman.latin.var.woff2"
-                    as="font"
-                    type="font/woff2"
-                    crossOrigin="anonymous"
-                />
-            </Head>
-            <MDXProvider components={components}>
-                <Component {...pageProps} />
-            </MDXProvider>
-        </>
-    )
+  return (
+    <>
+      <Head>
+        <link
+          rel="alternate"
+          type="application/rss+xml"
+          title="RSS"
+          href="/feed.xml"
+        />
+        <link
+          rel="preload"
+          href="/fonts/Inter-roman.latin.var.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
