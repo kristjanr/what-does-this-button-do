c7669032bc462eb0ba19618563e3e655df64d938
Kris <kristjan.roosild@pactum.com>
2023-03-15 12:00:52 +0200

    Add Youtube embedding component using GPT-4

--- next.config.js
+++ next.config.js
@@ -1,14 +1,2 @@
 const withNextra = require('nextra')('nextra-theme-blog', './theme.config.js')
-
-const withMDX = require('@next/mdx')({
-    extension: /\.mdx?$/,
-    options: {
-        remarkPlugins: [],
-        rehypePlugins: [],
-        components: {
-            youtube: './components/YouTube',
-        },
-    },
-})
-
-module.exports = withNextra(withMDX())
+module.exports = withNextra()
