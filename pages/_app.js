f37f6a367280c7bc67319810fbe367989039c7ef
kristjanro <kristjan.roosild@pactum.com>
2024-10-17 10:22:43 +0100

    google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,47 @@
-import 'nextra-theme-blog/style.css'
-import Head from 'next/head'
+import 'nextra-theme-blog/style.css';
+import Head from 'next/head';
+import Script from 'next/script';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
-import '../styles/main.css'
+import '../styles/main.css';
 
 export default function Nextra({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      window.gtag('config', 'G-33E976QP9Y', {
+        page_path: url,
+      });
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <>
+      {/* Google Analytics script */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-33E976QP9Y`}
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-33E976QP9Y', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
       <Head>
         <link
           rel="alternate"
@@ -23,5 +59,5 @@ export default function Nextra({ Component, pageProps }) {
       </Head>
       <Component {...pageProps} />
     </>
-  )
+  );
 }
